fced6e20c6b31776466a34b1978e5a55bfb583cb
Gowdra, Girish <girish.gowdra@intel.com>
2025-05-15 10:14:54 -0700

    fix rke2 cp errors

--- internal/server/kubeapi_handler.go
+++ internal/server/kubeapi_handler.go
@@ -14,19 +14,31 @@ import (
 
 	"github.com/atomix/dazl"
 	"github.com/gorilla/mux"
-	"github.com/open-edge-platform/cluster-connect-gateway/internal/metrics"
+	"k8s.io/apimachinery/pkg/util/proxy"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/transport"
+
+	"github.com/open-edge-platform/cluster-connect-gateway/internal/metrics"
 )
 
 const (
 	kubeApiEndpoint = "https://kubernetes.default.svc"
+	UpgradeHeader   = "Upgrade"
+	SpdyPrefix      = "spdy/"
+	Websocket       = "websocket"
 )
 
 var (
 	log     = dazl.GetPackageLogger()
 	clients = sync.Map{}
+	er      = &errorResponder{}
 )
+
+type Client struct {
+	httpClient *http.Client
+	restCfg    *rest.Config
+}
 
 type LoggingTransport struct {
 	Transport http.RoundTripper
@@ -47,7 +59,7 @@ func (lt *LoggingTransport) RoundTrip(req *http.Request) (*http.Response, error)
 }
 
 func (s *Server) KubeapiHandler(rw http.ResponseWriter, req *http.Request) {
-	start := time.Now() // TODO: refactor
+	start := time.Now()
 	timeout := req.URL.Query().Get("timeout")
 	if timeout == "" {
 		timeout = defaultTimeout
@@ -63,26 +75,71 @@ func (s *Server) KubeapiHandler(rw http.ResponseWriter, req *http.Request) {
 		rw.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	log.Debugf("[%s] REQ OK t=%s target: %s, req: %+v", tunnelID, timeout, target.String(), req)
+	log.Debugf("[%s] REQ OK t=%s %+v", tunnelID, timeout, req)
 
-	client, err := s.GetClientFromKubeconfig(tunnelID, timeout)
+	client, cfg, err := s.GetClientFromKubeconfig(tunnelID, timeout)
 	if err != nil {
 		log.Errorf("Error getting client for tunnel %s: %s", tunnelID, err)
 		rw.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
+	// Set common request fields
+	setRequestURL(req, target)
+
+	upgradeHeader := strings.ToLower(req.Header.Get(UpgradeHeader))
+	// There are three cases:
+	// 1. WebSocket upgrade or HTTP/1.1 or HTTP/2  request
+	// 2. SPDY Upgrade request
+	// 3. Any other request (not supported)
+	// The 'proxy.NewUpgradeAwareHandler' seems to have issues websocket upgrade requests and such traffic
+	// is pushed to be handled via 'httputil.NewSingleHostReverseProxy'
+	// The neatest way to have handled this is to use the 'proxy.NewUpgradeAwareHandler' for all requests,
+	// but what we have seen is that it doesn't work well with websocket upgrade requests. The solution
+	// here works for now.
+	if upgradeHeader == Websocket || upgradeHeader == "" {
+		s.handleWebSocketOrHTTP(rw, req, target, client, tunnelID)
+	} else if strings.HasPrefix(upgradeHeader, SpdyPrefix) {
+		s.handleSPDY(rw, req, target, client, cfg, tunnelID)
+	} else {
+		log.Warnf("[%s] Unsupported Upgrade header: %s", tunnelID, upgradeHeader)
+		http.Error(rw, "Unsupported Upgrade header", http.StatusBadRequest)
+		return
+	}
+
+	// Record metrics
+	recordMetrics(rw, start)
+}
+
+func setRequestURL(req *http.Request, target *url.URL) {
+	req.URL.Scheme = target.Scheme
+	req.Host = target.Host
+	req.URL.Path = target.Path
+}
+
+func recordMetrics(rw http.ResponseWriter, start time.Time) {
+	code := rw.Header().Get("Status")
+	if code == "" {
+		code = fmt.Sprintf("%d", http.StatusOK)
+	}
+	metrics.ProxiedHttpResponseCounter.WithLabelValues(code).Inc()
+
+	duration := time.Since(start).Seconds()
+	metrics.RequestLatency.Observe(duration)
+}
+
+func (s *Server) handleWebSocketOrHTTP(rw http.ResponseWriter, req *http.Request, target *url.URL, client *http.Client, tunnelID string) {
 	// Create proxy and set the transport to remotedialer client
-	proxy := httputil.NewSingleHostReverseProxy(target)
+	proxyHandler := httputil.NewSingleHostReverseProxy(target)
 
 	// Wrap the transport with LoggingTransport
-	proxy.Transport = &LoggingTransport{
+	proxyHandler.Transport = &LoggingTransport{
 		Transport: client.Transport, // Use the existing transport
 	}
 
 	// Modify the Director function to change the request
-	originalDirector := proxy.Director
-	proxy.Director = func(req *http.Request) {
+	originalDirector := proxyHandler.Director
+	proxyHandler.Director = func(req *http.Request) {
 		originalDirector(req)
 		req.URL.Scheme = target.Scheme
 		req.Host = target.Host
@@ -90,18 +147,18 @@ func (s *Server) KubeapiHandler(rw http.ResponseWriter, req *http.Request) {
 
 		// Preserve the Upgrade header for HTTP/1.1 requests
 		if req.ProtoMajor == 1 {
-			if upgrade := req.Header.Get("Upgrade"); upgrade != "" {
+			if upgrade := req.Header.Get(UpgradeHeader); upgrade != "" {
 				log.Debugf("[%s] Preserving Upgrade header: %s", tunnelID, upgrade)
 			}
 		} else {
 			// Remove the Upgrade header for HTTP/2 requests
-			req.Header.Del("Upgrade")
+			req.Header.Del(UpgradeHeader)
 		}
 
 		log.Debugf("[%s] REQ DONE: %v", tunnelID, req)
 	}
 
-	proxy.ModifyResponse = func(r *http.Response) error {
+	proxyHandler.ModifyResponse = func(r *http.Response) error {
 		if r != nil {
 			code := fmt.Sprintf("%d", r.StatusCode)
 			metrics.ProxiedHttpResponseCounter.WithLabelValues(code).Inc()
@@ -109,33 +166,52 @@ func (s *Server) KubeapiHandler(rw http.ResponseWriter, req *http.Request) {
 		return nil
 	}
 
-	proxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, err error) {
+	proxyHandler.ErrorHandler = func(w http.ResponseWriter, r *http.Request, err error) {
 		metrics.ProxiedHttpResponseCounter.WithLabelValues("502").Inc()
 		log.Debugf("[%s] REQ failed: %v", tunnelID, err)
 	}
 
-	proxy.ServeHTTP(rw, req)
-	log.Debugf("[%s] RESP RECEIVED: %v", tunnelID, rw)
-	duration := time.Since(start).Seconds()
-	metrics.RequestLatency.Observe(duration)
+	log.Debugf("[%s] REQ DONE: %+v", tunnelID, req)
+	proxyHandler.ServeHTTP(rw, req)
+	log.Debugf("[%s] RESP RECEIVED: %+v", tunnelID, rw)
 }
 
-func (s *Server) GetClientFromKubeconfig(tunnelID string, timeout string) (*http.Client, error) {
+func (s *Server) handleSPDY(rw http.ResponseWriter, req *http.Request, target *url.URL, client *http.Client, cfg *rest.Config, tunnelID string) {
+	// Create proxy and set the transport to remotedialer client
+	proxyHandler := proxy.NewUpgradeAwareHandler(target, client.Transport, false, false, er)
+
+	upgradeTransport, err := makeUpgradeTransport(cfg, client.Transport)
+	if err != nil {
+		return
+	}
+	proxyHandler.UpgradeTransport = upgradeTransport
+
+	req.URL.Scheme = target.Scheme
+	req.Host = target.Host
+	req.URL.Path = target.Path
+	log.Debugf("[%s] REQ DONE: %+v", tunnelID, req)
+
+	proxyHandler.ServeHTTP(rw, req)
+	log.Debugf("[%s] RESP RECEIVED: %+v", tunnelID, rw)
+
+}
+
+func (s *Server) GetClientFromKubeconfig(tunnelID, timeout string) (*http.Client, *rest.Config, error) {
 	// Check if the client is already cached
 	key := fmt.Sprintf("%s/%s", tunnelID, timeout)
 	client, ok := clients.Load(key)
 	if ok {
-		return client.(*http.Client), nil
+		return client.(*Client).httpClient, client.(*Client).restCfg, nil
 	}
 
-	start := time.Now() // TODO: refactor
+	start := time.Now()
 	// Get kubeconfig from the Secret if not
 	cfg, err := s.kubeclient.GetKubeconfig(tunnelID)
 	duration := time.Since(start).Seconds()
 	metrics.KubeconfigRetrievalDuration.Observe(duration)
 	if err != nil {
 		log.Errorf("Unable to get kubeconfig for %s: %v", tunnelID, err)
-		return nil, err
+		return nil, nil, err
 	}
 
 	// Set the server URL to the default kubeapi service URL
@@ -148,32 +224,43 @@ func (s *Server) GetClientFromKubeconfig(tunnelID string, timeout string) (*http
 	bytesCfg, err := clientcmd.Write(*cfg)
 	if err != nil {
 		log.Errorf("Unable to write kubeconfig for %s: %v", tunnelID, err)
-		return nil, err
+		return nil, nil, err
 	}
 
 	restCfg, err := clientcmd.RESTConfigFromKubeConfig(bytesCfg)
 	if err != nil || restCfg == nil {
 		log.Errorf("Unable to create client config %s: %v", tunnelID, err)
-		return nil, err
+		return nil, nil, err
 	}
 
 	restCfg.Dial = s.remotedialer.Dialer(tunnelID)
-
-	// Disable HTTP/2 in the transport
-	transport, err := rest.TransportFor(restCfg)
+	// Now create a new HTTP client with the rest config
+	httpClient, err := rest.HTTPClientFor(restCfg)
 	if err != nil {
-		log.Errorf("Unable to create transport for %s: %v", tunnelID, err)
+		log.Errorf("Unable to create HTTP client for %s: %v", tunnelID, err)
+		return nil, nil, err
+	}
+
+	newClient := &Client{
+		httpClient: httpClient,
+		restCfg:    restCfg,
+	}
+	clients.Store(key, newClient)
+	return httpClient, restCfg, nil
+}
+
+func makeUpgradeTransport(config *rest.Config, rt http.RoundTripper) (proxy.UpgradeRequestRoundTripper, error) {
+	transportConfig, err := config.TransportConfig()
+	if err != nil {
 		return nil, err
 	}
-	if httpTransport, ok := transport.(*http.Transport); ok {
-		httpTransport.ForceAttemptHTTP2 = false
+
+	upgrader, err := transport.HTTPWrappersForConfig(transportConfig, proxy.MirrorRequest)
+	if err != nil {
+		return nil, err
 	}
 
-	// Now create a new HTTP client with the rest config
-	httpClient := &http.Client{Transport: transport}
-
-	clients.Store(key, httpClient)
-	return httpClient, nil
+	return proxy.NewUpgradeRequestRoundTripper(rt, upgrader), nil
 }
 
 func (s *Server) cleanupUnusedHttpClients() {
